01f0e5d10e2d46eb8d35e90eac2da77f6dc30c37
Leogamermmmm <leo22.maron@hotmail.com>
2020-02-14 19:57:13 -0300

    Adiciona a aba de configurações

--- Frontend/mobile/src/pages/Home.js
+++ Frontend/mobile/src/pages/Home.js
@@ -5,10 +5,13 @@ import { useSafeArea } from 'react-native-safe-area-context';
 
 function Home(){
     const insets = useSafeArea();
+    let webview;
+
+    
 
     return(
         <View style={{flex: 1, marginTop: insets.top, backgroundColor: '#191919'}}>
-            <WebView source={{uri: "http://192.168.0.90"}}  />
+            <WebView source={{uri: "http://192.168.0.90"}} ref={ref => (webview = ref)} />
         </View>
     );
 }
--- Frontend/mobile/src/pages/Settings.js
+++ Frontend/mobile/src/pages/Settings.js
@@ -1,15 +1,49 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native';
 import { useSafeArea } from 'react-native-safe-area-context';
 
 function Settings(){
     const insets = useSafeArea();
-
+    const [pass, setPass] = useState('');
+    
     return(
-        <View style={{flex: 1, marginTop: insets.top, backgroundColor: '#191919'}}>
+        <View style={{flex: 1, marginTop: insets.top, backgroundColor: '#191919', alignItems: "center", justifyContent: 'center'}}>
+            <TextInput 
+                style={styles.input}
+                secureTextEntry={true}
+                placeholder="Senha"
+                value={pass}
+                onChangeText={setPass}
+            />
+
+            <TouchableOpacity style={styles.botao}>
+                <Text>Salvar</Text>
+            </TouchableOpacity>
 
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    input: {
+        backgroundColor: 'white',
+        width: 200,
+        height: 41,
+        textAlign: 'center',
+        borderRadius: 5,
+    },
+
+    botao: {
+        marginTop: 15,
+        backgroundColor: 'white',
+        width: 140,
+        height: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderColor: 'black',
+        borderWidth: 2,
+        borderRadius: 5,
+    }
+});
 
 export default Settings;
